5d23d46ed3feea6e0ae5177155a1c713ee0ac156
Seastiao <sebastiao_hndrv@gmail.com>
2025-03-28 18:17:17 -0300

    testes

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cors = require("cors");
 require('dotenv').config();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"], 
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],             
   allowedHeaders: ["Content-Type", "Authorization"],    
   credentials: true                                      
